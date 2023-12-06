1de7851972b24d2a456c3c336302aa885810b68c
Nguyễn Tiến Dũng-Intern <mrsoi2302@gmail.com>
2023-12-06 11:41:55 +0700

    complete

--- Front-End/mock/src/Components/RevenueChart.js
+++ Front-End/mock/src/Components/RevenueChart.js
@@ -49,14 +49,14 @@ const RevenueChart = () => {
             label: 'Giá trị',
             data:data,
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgb(255, 184, 228)',
+                'rgb(255, 184, 228)',
+                'rgb(255, 184, 228)',
+                'rgb(255, 184, 228)',
+                'rgb(255, 184, 228)',
+                'rgb(255, 184, 228)'
             ],
-            borderWidth: 2,
+            borderWidth: 3,
         }]
     };
 
